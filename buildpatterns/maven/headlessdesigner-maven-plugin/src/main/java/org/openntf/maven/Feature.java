c4a41c8fe465cf7a03e7dd7c00af5e67ae7a79db
Christian Guedemann <christian.guedemann@webgate.biz>
2014-05-01 14:27:50 +0200

    Fix URL not definable

--- buildpatterns/maven/headlessdesigner-maven-plugin/src/main/java/org/openntf/maven/Feature.java
+++ buildpatterns/maven/headlessdesigner-maven-plugin/src/main/java/org/openntf/maven/Feature.java
@@ -3,19 +3,19 @@ package org.openntf.maven;
 import org.apache.maven.plugins.annotations.Parameter;
 
 public class Feature {
-	@Parameter(property = "ddehd.path", alias = "url")
-	private String m_URL;
+	@Parameter(property = "ddehd.url", alias = "url")
+	private String m_Url;
 	@Parameter(property = "ddehd.featureId", alias = "featureId")
 	private String m_FeatureId;
 	@Parameter(property = "ddehd.version", alias = "version")
 	private String m_Version;
 
 	public String getURL() {
-		return m_URL;
+		return m_Url;
 	}
 
 	public void setURL(String uRL) {
-		m_URL = uRL;
+		m_Url = uRL;
 	}
 
 	public String getFeatureId() {
