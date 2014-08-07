684015e9a411c47768193003085a94e6689c18ce
Christian Guedemann <christian.guedemann@webgate.biz>
2014-08-07 11:25:16 +0200

    Some modifications

--- buildpatterns/maven/headlessdesigner-maven-plugin/src/main/java/org/openntf/maven/NsfDistribution.java
+++ buildpatterns/maven/headlessdesigner-maven-plugin/src/main/java/org/openntf/maven/NsfDistribution.java
@@ -1,29 +1,21 @@
 package org.openntf.maven;
 
-import java.io.File;
-
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
 import org.codehaus.plexus.util.StringUtils;
 
-@Mojo(name = "nsf")
-@Execute(goal = "nsf", phase = LifecyclePhase.PACKAGE)
-public class NsfDistribution extends AbstractMojo {
-	@Parameter(property = "ddehd.notesdata")
-	private String m_NotesData;
-	@Parameter(defaultValue = "${project.build.outputDirectory}")
-	private File m_OutputDir;
+@Mojo(name = "nsf-dist")
+@Execute(goal = "nsf-dist", phase = LifecyclePhase.PACKAGE)
+public class NsfDistribution extends AbstractDesignerPlugin {
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		getLog().info("Starting NSF Distribution");
-		getLog().info("Notes Data    =" + m_NotesData);
-		getLog().info("OutputDir     =" + m_OutputDir);
-		if (StringUtils.isEmpty(m_NotesData) || m_OutputDir == null) {
+		getLog().info("TargetDBName    =" + m_TargetDBName);
+		getLog().info("OutputDir     =" + m_TargetDir);
+		if (StringUtils.isEmpty(m_TargetDBName) || m_TargetDBName == null) {
 			getLog().info("NSF Distribution miss some configuration (ddehd.notesdata)");
 			throw new MojoExecutionException("NSF Distribution miss some configuration (ddehd.notesdata)");
 		}
