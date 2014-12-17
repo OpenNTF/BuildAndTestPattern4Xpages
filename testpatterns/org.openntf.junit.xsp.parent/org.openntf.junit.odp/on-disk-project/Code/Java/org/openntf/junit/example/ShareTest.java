8d5d4ef5968e7173fed293c0cc83da1c7f83adf9
Christian Guedemann <christian.guedemann@webgate.biz>
2014-12-17 07:50:50 +0100

    Sonar Integration

--- testpatterns/org.openntf.junit.xsp.parent/org.openntf.junit.odp/on-disk-project/Code/Java/org/openntf/junit/example/ShareTest.java
+++ testpatterns/org.openntf.junit.xsp.parent/org.openntf.junit.odp/on-disk-project/Code/Java/org/openntf/junit/example/ShareTest.java
@@ -4,6 +4,7 @@ import lotus.domino.Document;
 
 import org.junit.Test;
 import org.openntf.junit.example.bo.Share;
+import org.openntf.junit.xsp.easymock.EasyMockWrapper;
 
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
@@ -12,15 +13,15 @@ public class ShareTest {
 
 	@Test
 	public void testShareWithDocumentMock() {
-		Document docMock = createNiceMock(Document.class);
+		Document docMock = EasyMockWrapper.createNiceMock(Document.class);
 		try {
 			expect(docMock.getItemValueString("ShareName")).andReturn("WebGate");
 			expect(docMock.getItemValueInteger("Count")).andReturn(5);
 			expect(docMock.getItemValueInteger("PricePerShare")).andReturn(2870);
 			replay(docMock);
 			Share shareWebGate = Share.initFromDocument(docMock);
-			assertEquals("WebGate",shareWebGate.getShareName());
-			assertEquals( 5 * 2870, shareWebGate.getValue());
+			assertEquals("WebGate", shareWebGate.getShareName());
+			assertEquals(5 * 2870, shareWebGate.getValue());
 		} catch (Exception e) {
 			e.printStackTrace();
 			assertFalse(true);
